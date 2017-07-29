89932c97dda5143b5055a421c78d7407af3afb8d
Steven Okada <steven.okada@steven.okada-MBP>
2017-07-28 17:25:18 -0700

    messing around

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send("hi");
-});
+  response.send(app.get('port'));
+  });
 
-console.log(app.get('port'));
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
