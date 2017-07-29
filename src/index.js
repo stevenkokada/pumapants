31dceec1f0078ceae0595eb92b85be4dd2e00a0f
Steven Okada <steven.okada@steven.okada-MBP>
2017-07-28 17:27:47 -0700

    messing around

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.send(app.get('port'));
+  response.send(process.env.PORT);
   });
 
 
